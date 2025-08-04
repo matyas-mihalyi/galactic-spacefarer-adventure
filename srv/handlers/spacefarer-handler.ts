d0ac32eddae5b71ba43de4d5093a68a014a5bd2f
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2025-08-04 13:14:17 +0200

    fix association validation, add logs

--- srv/handlers/spacefarer-handler.ts
+++ srv/handlers/spacefarer-handler.ts
@@ -1,44 +1,45 @@
 import { Rank } from "#cds-models/GalacticSpacefarerService";
 import { GalacticSpacefarerService } from "../spacefarer-service";
 import cds from "@sap/cds";
+const logger = cds.log('spacefarer-handler')
 
 export class SpacefarerHandler {
   private static entities: any;
 
-  private constructor() {}
+  private constructor() { }
 
   static register(service: GalacticSpacefarerService) {
-    if (!this.entities) {
-      this.entities = service.entities;
+    if (!SpacefarerHandler.entities) {
+      SpacefarerHandler.entities = service.entities;
     }
     const { Spacefarers } = service.entities;
-    service.before("UPDATE", Spacefarers, this.beforeSpacefarerUpdate);
-    service.before("CREATE", Spacefarers, this.beforeSpacefarerCreate);
+    service.before("UPDATE", Spacefarers, SpacefarerHandler.beforeSpacefarerUpdate);
+    service.before("CREATE", Spacefarers, SpacefarerHandler.beforeSpacefarerCreate);
   }
 
   private static async beforeSpacefarerCreate(req: cds.Request) {
+    logger.log('Incoming create Spacefarer request', { data: req.data })
     const { collectedStardust, rank_ID, wormholeNavigationSkill } = req.data;
-    await this.validateAssociations(req);
-
+    await SpacefarerHandler.validateAssociations(req);
     if (rank_ID) {
-      await this.validateRank(collectedStardust, rank_ID);
+      await SpacefarerHandler.validateRank(collectedStardust, rank_ID);
     } else {
       const highestValidRank =
-        await this.findHighestValidRank(collectedStardust);
+        await SpacefarerHandler.findHighestValidRank(collectedStardust);
       req.data.rank_ID = highestValidRank.ID;
     }
 
     if (!wormholeNavigationSkill) {
-      req.data.wormholeNavigationSkill = this.enhanceWormholeNavigationSkills();
+      req.data.wormholeNavigationSkill = await SpacefarerHandler.enhanceWormholeNavigationSkills();
     }
   }
 
   private static async beforeSpacefarerUpdate(req: cds.Request) {
-    await this.validateAssociations(req);
+    await SpacefarerHandler.validateAssociations(req);
   }
 
   private static async validateRank(collectedStardust: number, rankId: string) {
-    const desiredRank = await SELECT.one.from(this.entities.Ranks)
+    const desiredRank = await SELECT.one.from(SpacefarerHandler.entities.Ranks)
       .where`ID = ${rankId}`;
     if (desiredRank.requiredStardust > collectedStardust) {
       throw new Error(
@@ -50,35 +51,39 @@ export class SpacefarerHandler {
   private static async findHighestValidRank(
     collectedStardust: number,
   ): Promise<Rank> {
-    return SELECT.one.from(this.entities.Ranks)
+    return SELECT.one.from(SpacefarerHandler.entities.Ranks)
       .where`requiredStardust <= ${collectedStardust}`
       .orderBy`requiredStardust desc`.limit(1);
   }
 
   private static async enhanceWormholeNavigationSkills(): Promise<number> {
-    console.log('Spacefarer candidate completed Wormhole Navigation Basic Training')
+    logger.log('Spacefarer candidate completed Wormhole Navigation Basic Training')
     return Promise.resolve(1);
   }
 
   private static async validateAssociations(req: cds.Request) {
     const { rank_ID, department_ID } = req.data;
-    const rank = await SELECT.one
-      .from(this.entities.Ranks)
-      .where({ ID: rank_ID });
-    if (!rank) {
-      req.reject(
-        400,
-        `Rank ${rank_ID} does not exits. Contact Admin to create it first.`,
-      );
+    if (typeof rank_ID === 'number') {
+      const rank = await SELECT.one
+        .from(SpacefarerHandler.entities.Ranks)
+        .where({ ID: rank_ID });
+      if (!rank) {
+        req.reject(
+          400,
+          `Rank ${rank_ID} does not exits. Contact Admin to create it first.`,
+        );
+      }
     }
-    const department = await SELECT.one
-      .from(this.entities.Departments)
-      .where({ ID: department_ID });
-    if (!department) {
-      req.reject(
-        400,
-        `Department ${department_ID} does not exits. Contact Admin to create it first.`,
-      );
+    if (typeof department_ID === "number") {
+      const department = await SELECT.one
+        .from(SpacefarerHandler.entities.Departments)
+        .where({ ID: department_ID });
+      if (!department) {
+        req.reject(
+          400,
+          `Department ${department_ID} does not exits. Contact Admin to create it first.`,
+        );
+      }
     }
   }
 }
